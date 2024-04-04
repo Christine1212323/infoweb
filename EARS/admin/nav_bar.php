0ec298d3ef8c8b8b0a0b732b7943fae0291f17ea
Christine1212323 <161209021+Christine1212323@users.noreply.github.com>
2024-04-04 08:35:46 +0800

    Add files via upload

---
+++ EARS/admin/nav_bar.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="navis.css">
+    <title>Deja Brew Website</title>
+</head>
+<body>
+ <div class="wrapper">
+    <nav class="navig">
+            <div class="navig-logo">
+                <a class="navig-logo2" href="home.php"> DEJA BREW CAFE </a>
+    
+            </div>
+            <div class="navig-menu" id="navigMenu">
+                <ul>
+                    <li><a href="home.php"class="link">Home</a></li>
+                    <li><a href="employee.php" class="link">Employee</a></li>
+                    <li><a href="attendance.php" class="link">Salary</a></li>
+                    <li><a href="attendance.php" class="link">Attendance</a></li>
+                    <li><a href="login2.php" class="link">Users</a></li>
+                    <li><a href="logout.php" class="link">Logout</a></li>
+                </ul>
+            </div>
+        </nav>
+        </div>
+    </body>
+          </html>
